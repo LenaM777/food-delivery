8d324c754ccb74f0c7c1aa00a0914a4c949717dc
Olena Mustafina <toysamiy.mail@gmail.com>
2025-02-05 13:13:53 +0200

    Update FoodItem: add counter button

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 
@@ -6,6 +6,7 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
+    
 
     const contextValue = {
         food_list
