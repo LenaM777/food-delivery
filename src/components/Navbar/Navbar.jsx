da5fdc575247307bb42208231f4829c07ce7a143
Olena Mustafina <toysamiy.mail@gmail.com>
2025-02-05 17:32:47 +0200

    Update Navbar: add links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
@@ -9,10 +10,10 @@ const Navbar = () => {
     <div className='navbar'>
         <img src={assets.logo} alt="" className='logo' />
         <ul className='navbar-menu'>
-            <li onClick={()=>setMenu("home")} className={menu==="home" ? "activ" : ""}>home</li>
-            <li onClick={()=>setMenu("menu")} className={menu==="menu" ? "activ" : ""}>menu</li>
-            <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app" ? "activ" : ""}>mobile-app</li>
-            <li onClick={()=>setMenu("contact us")} className={menu==="contact us" ? "activ" : ""}>contact us</li>
+            <Link to='/' onClick={()=>setMenu("home")} className={menu==="home" ? "activ" : ""}>home</Link>
+            <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu" ? "activ" : ""}>menu</a>
+            <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app" ? "activ" : ""}>mobile-app</a>
+            <a href='#footer' onClick={()=>setMenu("contact us")} className={menu==="contact us" ? "activ" : ""}>contact us</a>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
